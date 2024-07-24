a6bdefa5de0f8dc8b7162f0900a5af3ededfd250
JoseDev <joseluisdp297@gmail.com>
2024-07-24 19:22:51 +0100

    Create view using modelViewSet

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+# Haciendolo con una funcion:
+
+# @api_view(['GET']) el decorador @api_view es necesario para especificar qué métodos HTTP (GET, POST, etc.) deben ser manejados por esa vista.
+# def task_list(request):
+#     tasks = Task.objects.all()
+#     serializer = TaskSerializer(tasks, many=True)
+#     return JsonResponse(serializer.data, safe=False)
