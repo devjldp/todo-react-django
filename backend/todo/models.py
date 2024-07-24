ec883f6a3aff5b9d4ea7acc8a47dc1df2c1457c5
JoseDev <joseluisdp297@gmail.com>
2024-07-24 19:03:45 +0100

    Create model task

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    task = models.CharField(max_length=200)
+    completed = models.BooleanField()
+
+    def __str__(self):
+        return self.task
